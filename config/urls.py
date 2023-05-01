7e6a25bbc8f36ae277a4db6db81af338c53429de
Gachal29 <taisuke0129t@gmail.com>
2023-05-01 17:01:35 +0900

    top.htmlの修正, SeatDecideViewの作成

--- config/urls.py
+++ config/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name="logout"),
     path('', laboratory_views.TopView.as_view(), name="top"),
     path('seat_confirmation/', attendance_views.SeatConfirmationView.as_view(), name="seat_confirm"),
+    path('seat_decide/', laboratory_views.SeatDecideView.as_view(), name="seat-decide"),
 ]
--- laboratory/views.py
+++ laboratory/views.py
@@ -1,6 +1,23 @@
+from typing import Any, Dict
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from laboratory.models import Laboratory
 
 
 class TopView(LoginRequiredMixin, TemplateView):
     template_name = "top.html"
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+        context["user"] = user
+        try:
+            context["laboratory"] = Laboratory.objects.get(admin_user=user)
+        except:
+            pass
+
+        return context
+
+
+class SeatDecideView(LoginRequiredMixin, TemplateView):
+    template_name = "seat_decide.html"
